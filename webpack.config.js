2498da3f9b174f21c327523fe1e03d3903c021b9
hernan <hcastillaq@gmail.com>
2017-11-22 16:49:27 -0500

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,22 @@ const config =
   output:{
     path: __dirname + '/dist/js/',
     filename: '[name].js'
+  },
+  module:{
+    rules:[
+      {
+        test:/\.js$/,
+        use:[
+          {
+            loader:'babel-loader',
+            options: {
+              presets: ['babel-preset-env']
+            }
+          }
+        ],
+        exclude:/node_modules/
+      }
+    ]
   }
 }
 module.exports  = config;
