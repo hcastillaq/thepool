2444fae67581c0c1af7eaea7b1c4fb3a010de794
Hernan <hcastillaq@gmail.com>
2019-06-13 00:06:07 -0500

    creado result page y mejorando la manera de cambiar a la pagina

---
+++ src/pages/results.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class ResultsPage extends React.component
+{
+	render()
+	{
+		return(
+			<div>
+				Results page
+			</div>
+		)
+	}
+}
+
+default export ResultsPage;
